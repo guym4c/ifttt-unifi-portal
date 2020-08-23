25b6f52f292503abaabd1d17e8eecd0837112a22
Guy Mac <hello@guymac.eu>
2020-08-23 12:34:43 +0100

    :sparkles: tidy poll function

--- functions/poll.js
+++ functions/poll.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-const { env } = require('process');
 const redis = require('../helpers/redis');
 const { CONNECTION_APPROVED } = require('../constants/status');
 const { AUTH_REQUEST_ERROR } = require('../constants/error');
