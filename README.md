4c6945dc29880cfc7317305c92ed61164e2486aa
Guy Mac <hello@guymac.eu>
2020-09-27 16:23:24 +0200

    :recycle: use hooks

--- README.md
+++ README.md
@@ -13,9 +13,14 @@ You will need:
 Set the provided environment variables, link up a domain and you're good to go.
 
 ## Linking to IFTTT
-Oh, one more thing. You'll need to set up an IFTTT recipe. 
+On the IFTTT side, you'll need to set up an applet to approve connecting users.
 
 Set the relevant env variable to what you're named your maker event trigger. You now need a way of authorising guests - probably an IFTTT app notification. You're provided with the guest's name (as they gave it) as the ingredient `value1`, and the approval link as `value2`.
+
+## Adding to Unifi
+In the *general hotspot* settings on your network controller, set the *authentication mode* to *external portal server* and insert Netlify's external load balancer IP of 104.198.14.52.
+
+Then, in the *advanced* screen, enable *redirect using hostname*, and set the host to the domain that points to this application's Netlify deployment.
 
 ## Redis
 The storage the app requires on Redis is minimal - the 30MB that [RedisLabs give you free](https://redislabs.com/redis-enterprise-cloud/pricing/) is easily sufficient.
--- client/src/ConnectForm.jsx
+++ client/src/ConnectForm.jsx
@@ -1,97 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { RotateSpinner } from 'react-spinners-kit';
 import http from 'axios';
 
 const DEFAULT_REDIRECT_URL = 'https://google.com';
 
-class ConnectForm extends React.Component {
-  constructor(props) {
-    super(props);
+const refresh = () => window.location.reload();
 
-    this.state = {
-      connecting: false,
-      name: '',
-    };
-  }
+const poll = (id) => {
+  window.setInterval(() => {
+    http.post(`https://${process.env.REACT_APP_HOST}/.netlify/functions/poll`, { id })
+      .then(({ data: { connected } }) => {
+        if (connected === true) {
+          window.location.replace(
+            new URLSearchParams(window.location.search).get('url')
+            || DEFAULT_REDIRECT_URL
+          );
+        }
+      })
+      .catch(refresh);
+  }, 3 * 1000);
+};
 
-  onNameChange = ({ target: { value: name = '' } }) => {
+const ConnectForm = () => {
+  const [connecting, setConnecting] = useState(false);
+  const [name, setName] = useState('');
+
+  const onNameChange = ({ target: { value = '' } }) => {
     if (
-      name === ''
-      || name.match(/^[A-Za-z][\sA-Za-z]*$/) !== null
+      value === ''
+      || value.match(/^[A-Za-z][\sA-Za-z]*$/) !== null
     ) {
-      this.setState({ name });
+      setName(value);
     }
   };
 
-  onSubmit = () => {
-    this.setState({ connecting: true });
-
-    this.requestAuth()
-      .then(({ data: { pollId } }) => {
-        this.poll(pollId);
-      })
-      .catch(this.refresh);
-  };
-
-  refresh = () => window.location.reload();
-
-  requestAuth = () => {
-    const { name } = this.state;
-    return http.post(`https://${process.env.REACT_APP_HOST}/.netlify/functions/requestAuthorisation`, {
+  const requestAuth = () => http.post(
+    `https://${process.env.REACT_APP_HOST}/.netlify/functions/requestAuthorisation`,
+    {
       name,
       mac: new URLSearchParams(window.location.search).get('id'),
-    });
+    }
+  );
+
+  const onSubmit = () => {
+    setConnecting(true);
+
+    requestAuth()
+      .then(({ data: { pollId } }) => {
+        poll(pollId);
+      })
+      .catch(refresh);
   };
 
-  poll = (id) => {
-    window.setInterval(() => {
-      http.post(`https://${process.env.REACT_APP_HOST}/.netlify/functions/poll`, { id })
-        .then(({ data: { connected } }) => {
-          if (connected === true) {
-            window.location.replace(
-              new URLSearchParams(window.location.search).get('url')
-              || DEFAULT_REDIRECT_URL
-            );
-          }
-        })
-        .catch(this.refresh);
-    }, 3 * 1000);
-  };
-
-  render() {
-    const { name, connecting } = this.state;
-    return connecting
-      ? (
-        <div className="d-flex flex-column align-items-center">
-          <RotateSpinner />
-          <h2 className="mt-2">Connecting</h2>
-        </div>
-      )
-      : (
-        <React.Fragment>
-          <h1>Join network</h1>
-          <Form>
-            <Form.Group controlId="name">
-              <Form.Label>What&rsquo;s your name?</Form.Label>
-              <Form.Control
-                value={name}
-                onChange={this.onNameChange}
-                autofocus={true}
-              />
-            </Form.Group>
-            <Button
-              type="submit"
-              variant="success"
-              onClick={this.onSubmit}
-              disabled={name.length === 0}
-            >
-              Connect
-            </Button>
-          </Form>
-        </React.Fragment>
-      );
-  }
-}
+  return connecting
+    ? (
+      <div className="d-flex flex-column align-items-center">
+        <RotateSpinner />
+        <h2 className="mt-2">Connecting</h2>
+      </div>
+    )
+    : (
+      <React.Fragment>
+        <h1>Join network</h1>
+        <Form>
+          <Form.Group controlId="name">
+            <Form.Label>What&rsquo;s your name?</Form.Label>
+            <Form.Control
+              value={name}
+              onChange={onNameChange}
+              autofocus={true}
+            />
+          </Form.Group>
+          <Button
+            type="submit"
+            variant="success"
+            onClick={onSubmit}
+            disabled={name.length === 0}
+          >
+            Connect
+          </Button>
+        </Form>
+      </React.Fragment>
+    );
+};
 
 export default ConnectForm;
